e57843361971fddf028a23e9a2f90516434d44ff
Dayne Cordray <daynecc6@gmail.com>
2023-05-15 17:30:37 -0500

    imports

--- culture-app/src/pages/Purpose/StorySubmission.js
+++ culture-app/src/pages/Purpose/StorySubmission.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Box, TextField, Button } from "@mui/material";
 
 const StorySubmission = () => {
   const [story, setStory] = useState("");
